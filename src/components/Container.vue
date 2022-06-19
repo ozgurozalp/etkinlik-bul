573206d63925341c6d6659061c0b8999512c1ab9
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-06-19 10:31:45 +0300

    Create Container.vue

---
+++ src/components/Container.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="container">
+		<slot />
+	</div>
+</template>
+
+<style scoped lang="postcss">
+.container {
+	width: 100%;
+	margin-inline: auto;
+
+	@media (--sm) {
+		max-width: 640px;
+	}
+
+	@media (--md) {
+		max-width: 768px;
+	}
+
+	@media (--lg) {
+		max-width: 1024px;
+	}
+
+	@media (--xl) {
+		max-width: 1280px;
+	}
+
+	@media (--2xl) {
+		max-width: 1536px;
+	}
+}
+</style>
